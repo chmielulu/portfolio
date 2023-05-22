c8d7ffee2b3fb6a7675eecc9824dce5a8dad0c64
Jakub Chmielewski <jakubchmielewski80@gmail.com>
2023-05-22 23:39:42 +0200

    hide service prices

--- config/services.ts
+++ config/services.ts
@@ -3,42 +3,42 @@ export const SERVICES = [
     name: "Strony internetowe",
     content:
       "Zaprogramowanie strony internetowej wraz ze stworzeniem indywidualnego projektu graficznego oraz systemem CMS.",
-    pricing: "Od 3500 PLN",
+    pricing: "Poproś o wycenę",
     image: "computer-front-gradient.png",
   },
   {
     name: "Sklepy internetowe",
     content:
       "Zaprogramowanie sklepu internetowego obsługującego płatności internetowe razem z systemem CMS/CRM.",
-    pricing: "Od 7000 PLN",
+    pricing: "Poproś o wycenę",
     image: "dollar-front-gradient.png",
   },
   {
     name: "Blog",
     content:
       "Zaprogramowanie nowoczesnego bloga lub serwisu informacyjnego razem z systemem CMS.",
-    pricing: "Od 4000 PLN",
+    pricing: "Poproś o wycenę",
     image: "tea-cup-front-gradient.png",
   },
   {
     name: "Projekt graficzny",
     content:
       "Stworzenie indywidualnego projektu graficznego strony internetowej, sklepu internetowego, bloga lub aplikacji na telefon.",
-    pricing: "Od 1000 PLN",
+    pricing: "Poproś o wycenę",
     image: "figma-front-gradient.png",
   },
   {
     name: "Oprogramowanie dla firm",
     content:
       "Zaprogramowanie systemu zamówień w sklepach stacjonarnych i restauracjach lub innych tego typu miejscach.",
-    pricing: "Od 7000 PLN",
+    pricing: "Poproś o wycenę",
     image: "calculator-front-gradient.png",
   },
   {
     name: "Aplikacje na telefon",
     content:
       "Zaprogramowanie  aplikacji na telefon z systemem Android/iOS razem z projektem graficznym.",
-    pricing: "Od 7000 PLN",
+    pricing: "Poproś o wycenę",
     image: "mobile-front-gradient.png",
   },
 ];
