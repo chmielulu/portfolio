4459b1956f865561217588e9fc461d5961575e49
Jakub Chmielewski <jakubchmielewski80@gmail.com>
2023-07-13 20:59:07 +0200

    disable plausible analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,11 +18,12 @@ function Document() {
           href="https://fonts.bunny.net/css?family=raleway:400,500,600,700,800"
           rel="stylesheet"
         />
-        <script
-          defer
-          data-domain="jakubprogramista.dev"
-          src="https://plausible.jacobprogrammer.dev/js/script.js"
-        />
+        {/*^TODO VPS EXPIRED*/}
+        {/*<script*/}
+        {/*  defer*/}
+        {/*  data-domain="jakubprogramista.dev"*/}
+        {/*  src="https://plausible.jacobprogrammer.dev/js/script.js"*/}
+        {/*/>*/}
       </Head>
       <body className="with-background">
         <Main />
