85acb59deabbe904bec2229560af240c123ca8a8
Jakub Chmielewski <jakubchmielewski80@gmail.com>
2023-07-21 16:18:00 +0200

    Remove "Time4house" from other projects

--- components/molecules/OtherProjects/OtherProjects.tsx
+++ components/molecules/OtherProjects/OtherProjects.tsx
@@ -11,37 +11,33 @@ import otherProjectImageSrc3 from "../../../assets/images/other-project-3.png";
 import otherProjectImageSrc4 from "../../../assets/images/other-project-4.png";
 import otherProjectImageSrc5 from "../../../assets/images/other-project-5.png";
 import otherProjectImageSrc6 from "../../../assets/images/other-project-6.png";
-import otherProjectImageSrc7 from "../../../assets/images/other-project-7.png";
 
-const OtherProjects: FC<Props> = () => {
-  const images = [
-    { src: otherProjectImageSrc1, href: "https://bgkinvestment.pl" },
-    { src: otherProjectImageSrc2, href: "https://palacowapark.pl" },
-    { src: otherProjectImageSrc3, href: "https://mdwgroup.pl" },
-    { src: otherProjectImageSrc4, href: "https://osiedlepodmiastem.pl" },
-    { src: otherProjectImageSrc5, href: "https://nowa-farma.pl" },
-    { src: otherProjectImageSrc6, href: "https://time4house.com" },
-    { src: otherProjectImageSrc7, href: "https://olenkipark.pl" },
-  ];
+const images = [
+  { src: otherProjectImageSrc1, href: "https://bgkinvestment.pl" },
+  { src: otherProjectImageSrc2, href: "https://palacowapark.pl" },
+  { src: otherProjectImageSrc3, href: "https://mdwgroup.pl" },
+  { src: otherProjectImageSrc4, href: "https://osiedlepodmiastem.pl" },
+  { src: otherProjectImageSrc5, href: "https://nowa-farma.pl" },
+  { src: otherProjectImageSrc6, href: "https://olenkipark.pl" },
+];
 
-  return (
-    <StyledWrapper>
-      <StyledSectionHeadline headline="Inne projekty" subHeadline="Portfolio" />
-      {images.map((image, index) => (
-        <StyledOtherProjectWrapper key={index}>
-          <a href={image.href} rel="noreferrer noopener" target="_blank">
-            <StyledOtherProject
-              src={image.src}
-              quality={80}
-              placeholder="blur"
-              alt=""
-            />
-          </a>
-        </StyledOtherProjectWrapper>
-      ))}
-    </StyledWrapper>
-  );
-};
+const OtherProjects: FC<Props> = () => (
+  <StyledWrapper>
+    <StyledSectionHeadline headline="Inne projekty" subHeadline="Portfolio" />
+    {images.map((image, index) => (
+      <StyledOtherProjectWrapper key={index}>
+        <a href={image.href} rel="noreferrer noopener" target="_blank">
+          <StyledOtherProject
+            src={image.src}
+            quality={80}
+            placeholder="blur"
+            alt=""
+          />
+        </a>
+      </StyledOtherProjectWrapper>
+    ))}
+  </StyledWrapper>
+);
 
 interface Props {}
 
